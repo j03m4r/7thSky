f7f599ed6ec3d8999b541eeabc1311bd0506b469
j03m4r <joeemarr12345@gmail.com>
2023-08-07 13:55:31 -0500

    Trying to get 3JS to work pt4

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,7 @@
 import PageContent from "./components/PageContent";
 
+export const revalidate = 0;
+
 export default function Home() {
   return (
     <div className="flex h-full w-full justify-center items-center">
