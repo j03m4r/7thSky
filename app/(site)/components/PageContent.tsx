8bb5ebb65aad28460bbc047830ca3368442ab7ae
j03m4r <joeemarr12345@gmail.com>
2023-08-07 13:43:57 -0500

    Trying to get 3JS to work

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -18,16 +18,16 @@ const PageContent = () => {
             <Suspense fallback={null}>
                 <Cloud position={[0, 16, -15]} speed={0.4} opacity={0.5} />
                 <Cloud position={[15, 16, -10]} speed={0.4} opacity={0.5} />
-                <Cloud position={[6, 2, -5]} speed={0.4} opacity={1} />
+                {/* <Cloud position={[6, 2, -5]} speed={0.4} opacity={1} />
                 <Cloud position={[17, 15, -8]} speed={0.4} opacity={.6} />
                 <Cloud position={[10, 10, -7]} speed={0.4} opacity={1} />
-                <Cloud position={[-4, -5, -4]} speed={0.4} opacity={.5} />
-                <Cloud position={[-8, -8, 0]} speed={0.4} opacity={.6} />
+                <Cloud position={[-4, -5, -4]} speed={0.4} opacity={.5} /> */}
+                {/* <Cloud position={[-8, -8, 0]} speed={0.4} opacity={.6} />
                 <Cloud position={[-10, 0, -5]} speed={0.4} opacity={.75} />
                 <Cloud position={[-10, 10, -5]} speed={0.3} opacity={.5} />
                 <Cloud position={[8, 2, -30]} speed={0.3} opacity={1} />
                 <Cloud position={[-8, 0, -20]} speed={0.3} opacity={1} />
-                <Cloud position={[10, -6, 0]} speed={0.2} opacity={.5} />
+                <Cloud position={[10, -6, 0]} speed={0.2} opacity={.5} /> */}
                 <Cloud position={[15, -2, -1]} speed={0.2} opacity={.75} />
                 <Cloud position={[0, 7, -20]} speed={0.2} opacity={1} />
                 <Cloud position={[-10, 10, -15]} speed={0.2} opacity={.5} />
